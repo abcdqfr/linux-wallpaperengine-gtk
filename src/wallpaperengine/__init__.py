fb8340a49ec96b84a660be9e38373536efa947fd
Brandon <brandonhendershot@gmail.com>
2025-09-03 08:30:48 -0700

    Remove cruft files and clean up repository
    - Removed unnecessary commit_message.txt, requirements.txt
    - Removed broken modular src/wallpaperengine/ structure
    - Removed incomplete test files
    - Updated wallpaper-engine.log

--- src/wallpaperengine/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Linux Wallpaper Engine GTK Frontend Package"""
-from .main import PathManager, SettingsManager, WallpaperEngine, MainWindow, SettingsDialog, main
-
-__version__ = '0.1.0'
-__all__ = ['PathManager', 'SettingsManager', 'WallpaperEngine', 'MainWindow', 'SettingsDialog', 'main'] 
